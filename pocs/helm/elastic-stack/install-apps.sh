d604a70c555573d3922d45cb3f1009c0713658a7
hugo iguana <hugo.iguanaa@gmail.com>
2023-03-02 16:43:41 +0000

    Elastic-stack. #2

--- pocs/helm/elastic-stack/install-apps.sh
+++ pocs/helm/elastic-stack/install-apps.sh
@@ -4,11 +4,11 @@ kind delete cluster --name helm-elastic-stack
 kind create cluster --config kind.yml --name helm-elastic-stack
 kubectl config use-context kind-helm-elastic-stack
 kubectl create namespace app
-#kubectl create namespace elastic
+kubectl create namespace monitoring
 
 helm install app ./app-chart -n app
 
-#helm dependency build ./cassandra-chart
-#helm install cassandra-chart ./cassandra-chart -n database
-#helm upgrade cassandra-chart ./cassandra-chart -n database
+helm dependency build ./elastic-stack-chart
+helm install elastic-stack ./elastic-stack-chart -n monitoring
+#helm upgrade elastic-stack ./elastic-stack-chart -n monitoring
 
